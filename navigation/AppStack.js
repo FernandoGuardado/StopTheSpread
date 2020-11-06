aa0a67cd6990eeb3dd6b2c697986d800643ea18d
My Hao Ho <myhaoho@Mys-MacBook-Pro.local>
2020-11-05 19:15:38 -0700

    added maps, resource, heatmap

--- navigation/AppStack.js
+++ navigation/AppStack.js
@@ -1,16 +1,37 @@
 import React , { useState } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
+import MapScreen from '../screens/MapScreen';
+import ResourceScreen from '../screens/ResourceScreen';
+
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const AppStack = () => {
     return (
-        <Stack.Navigator>
-            <Stack.Screen name='Home' component={HomeScreen}/>
-        </Stack.Navigator>
-
+      <Tab.Navigator
+        tabBarOptions={{
+          activeTintColor: '#2e64e5',
+          labelStyle: {
+            fontSize: 20,
+        },
+        }}>
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+        />
+        <Tab.Screen
+          name="Map"
+          component={MapScreen}
+        />
+        <Tab.Screen
+          name="Resource"
+          component={ResourceScreen}
+        />
+      </Tab.Navigator>
     );
-}
-
-export default AppStack;
+  }
+  
+  export default AppStack;
